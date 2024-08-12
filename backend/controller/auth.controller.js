870526aeb1321f25cebd9e63cf1687481d5cbc88
Srivastava Mohit <mshrivastava43082@gmail.com>
2024-08-12 15:38:45 +0530

    signup, login, logout auth route

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -1,5 +1,90 @@
-export const signup = (req, res) => {}
+import User from '../models/user.model.js'
+import bcryptjs from 'bcryptjs'
+import jwt from 'jsonwebtoken'
+import { errHandler } from '../utils/error.js'
 
-export const login = (req, res) => {}
+export const signup = async(req, res, next) => {
+    const {username, email, password, confirmPassword, gender} = req.body
 
-export const logout = (req, res) => {}
+    let validUser
+
+    validUser = await User.findOne({email})
+
+    if(validUser) {
+        return next(errHandler(400, "User already exists"))
+    }
+
+    if(password !== confirmPassword) {
+        return next(errHandler(400, "Password don't match"))
+    }
+
+    const hashshedPassword = bcryptjs.hashSync(password, 7)
+
+    const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`
+    const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`
+
+    const newUSer = new User({
+        username,
+        email,
+        password: hashshedPassword,
+        gender,
+        profilePic: gender === "male" ? boyProfilePic : girlProfilePic,
+    })
+
+    try {
+        // generate jwt token
+        const token = jwt.sign({id: newUSer._id}, process.env.JWT_SECRET)
+
+        await newUSer.save()
+
+        res.cookie("access_token", token, {httpOnly: true}).status(201).json({
+            _id: newUSer._id,
+            username: newUSer.username,
+            email: newUSer.email,
+            profilePic: newUSer.profilePic,
+        })
+    } catch (error) {
+        next(error)
+    }
+}
+
+export const login = async(req, res, next) => {
+    try {
+        const {email, password} = req.body
+
+        const validUser = await User.findOne({email})
+
+        if(!validUser) {
+            return next(errHandler(404, "User not found"))
+        }
+        const validPassword = bcryptjs.compareSync(password, validUser.password)
+
+        if(!validPassword) {
+            return next(errHandler(401, "Wrong Password"))
+        }
+
+        const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET)
+
+        res.cookie("access_token", token, {httpOnly: true}).status(200).json({
+            _id: validUser._id,
+            username: validUser.username,
+            email: validUser.email,
+            profilePic: validUser.profilePic,
+        })
+    } catch (error) {
+        next(error)
+    }
+}
+
+export const logout = (req, res, next) => {
+    try {
+        res.clearCookie("access_token")
+
+        res.status(200).json({
+            message: "User successfully logout",
+        })
+    } catch (error) {
+        next(error)
+    }
+}
+
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import mongoose from 'mongoose'
+import cookieParser from 'cookie-parser'
 
 dotenv.config()
 
@@ -12,7 +13,11 @@ mongoose.connect(process.env.MONGO_URI).then( () => {
 
 const app = express() 
 
+app.use(express.json())
+
 const PORT = process.env.PORT || 3000
+
+app.use(cookieParser())
 
 app.get("/", (req, res) => {
     res.send("Hello World!!")
@@ -25,4 +30,16 @@ app.use("/api/auth", authRoutes)
 
 app.listen(PORT, () => {
     console.log("Server is running on port " + PORT)
+})
+
+// error handler
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal server error"
+
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    })
 })
