73ccaa29677a407e0fb6bf402e360761eebf2eee
Srivastava Mohit <mshrivastava43082@gmail.com>
2024-08-14 23:49:43 +0530

    getmessage api route

--- backend/routes/message.routes.js
+++ backend/routes/message.routes.js
@@ -1,11 +1,13 @@
 import express from 'express'
 import { isAuthenticate } from '../middleware/verifytoken.js'
 import { sendMessage } from "../controller/message.controller.js"
+import { getMessage } from "../controller/message.controller.js"
 
 const router = express.Router()
 
 
 // router.get("/:id", isAuthenticate, getMessage)
+router.get("/:id", isAuthenticate, getMessage)
 router.post("/send/:id", isAuthenticate, sendMessage)
 
 export default router
