98bf0b858561d0b5347267d77cb0bab7569b3a77
Srivastava Mohit <mshrivastava43082@gmail.com>
2024-08-16 01:13:03 +0530

    message container UI

---
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import MessageField from './MessageField'
+import Messages from './Messages'
+
+const MessageContainer = () => {
+  return (
+    <div className='md:min-w-[450px]  flex flex-col'>
+        <>
+            <div className='bg-slate-500 px-4 py-2 mb-2'>
+                <span className='text-gray-900 font-bold'>John Snow</span>
+            </div>
+            <Messages/>
+            <MessageField/>
+        </>
+    </div>
+  )
+}
+
+export default MessageContainer
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Sidebar from '../components/sidebar/Sidebar'
+import MessageContainer from '../components/messages/MessageContainer'
 
 const Home = () => {
   return (
     <div className='flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bh-gray-200'>
       <Sidebar/>
+      <MessageContainer/>
     </div>
   )
 }
