b2136d2cd449227c9f0805077b2ec08474807178
Srivastava Mohit <mshrivastava43082@gmail.com>
2024-08-18 00:28:57 +0530

    get Message funtionality

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -1,18 +1,32 @@
 import React from 'react'
 import userAvatar from '../../assets/user.png'
+import { useAuthContext } from '../../context/AuthContext'
+import useConversation from '../../zustand/useConversation'
+import { formatTime } from '../../utlis/formatTime'
 
-const Message = () => {
+const Message = ({ message }) => {
+  console.log("Message - ", message);
+  
+  const {authUser} = useAuthContext()
+  const {selectedConversation} = useConversation()
+  const messageFromMe = message.senderId === authUser._id 
+  const chatClassName = messageFromMe ? "chat-end" : "chat-start"
+  const profilePic = messageFromMe ? authUser.profilePic : selectedConversation?.profilePic
+  const msgBgcolor = messageFromMe ? "bg-green-500" : ""
+  const formattedTime = formatTime(message.createdAt)
+
+
   return (
-    <div className='chat chat-end '>
+    <div className={`chat ${chatClassName}`}>
       <div className="chat-image avatar">
         <div className='w-10 rounded-full'>
-          <img src={userAvatar} alt="user avatar" />
+          <img src={profilePic} alt="user avatar" />
         </div>
       </div>
 
-      <div className="chat-bubble text-white bg-blue-500">Hello</div>
+      <div className={`chat-bubble text-white ${msgBgcolor}`}>{message.message}</div>
 
-      <div className="chat-footer opacity-50 text-xs flex-1 gap-1 items-center text-slate-950">11:11</div>
+      <div className="chat-footer opacity-50 text-xs flex-1 gap-1 items-center text-slate-950">{formattedTime}</div>
     </div>
   )
 }
