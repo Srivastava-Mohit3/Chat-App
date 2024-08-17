40b4dc9c26a34a2183be157fbb7089c690d4c0e7
Srivastava Mohit <mshrivastava43082@gmail.com>
2024-08-17 20:21:21 +0530

    get Conversations functionality

--- frontend/src/components/sidebar/Conversations.jsx
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -1,19 +1,17 @@
 import React from 'react'  
 import ConversationList from './ConversationList'
+import useGetConversation from '../../hooks/useGetConversation'
 
 const Conversations = () => {
+  const {loading, conversations} = useGetConversation()
+
   return (
     <div className='py-2 flex flex-col overflow-auto'>
-      <ConversationList/>
-      <ConversationList/>
-      <ConversationList/>
-      <ConversationList/>
-      <ConversationList/>
-      <ConversationList/>
-      <ConversationList/>
-      <ConversationList/>
+      {conversations.map((conversation, index) => (
+        <ConversationList key={conversation._id} conversation={conversation} lastIndex={index === conversation.length - 1} />
+      ))}
+      {loading ? <span className="loading loading-spinner"></span> : null}
     </div>
   )
 }
-
 export default Conversations
---
+++ frontend/src/zustand/useConversation.js
@@ -0,0 +1,25 @@
+import {create} from 'zustand'
+
+const useConversation = create((set) => ({
+    selectedConversation: null,
+    setSelectedConversation: (selectedConversation) => set({selectedConversation}),
+    
+    messages: [],
+    setMessages: (messages) => ({messages}),
+}))
+
+export default useConversation
+
+
+// import { create } from "zustand"
+
+// const useConversation = create((set) => ({
+//   selectedConversation: null,
+//   setSelectedConversation: (selectedConversation) =>
+//     set({ selectedConversation }),
+
+//   messages: [],
+//   setMessages: (messages) => set({ messages }),
+// }))
+
+// export default useConversation
