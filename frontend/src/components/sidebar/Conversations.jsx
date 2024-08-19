8986d3d3aa2f8180bb8c3cc59e5f86b36d484f77
Srivastava Mohit <mshrivastava43082@gmail.com>
2024-08-19 17:04:30 +0530

    some issue resolved

--- frontend/src/components/sidebar/Conversations.jsx
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -1,39 +1,40 @@
-import React from 'react'  
-import ConversationList from './ConversationList'
-import useGetConversation from '../../hooks/useGetConversation'
-
-const Conversations = () => {
-  const {loading, conversations} = useGetConversation()
-  console.log(conversations);
-  
-
-  return (
-    <div className='py-2 flex flex-col overflow-auto'>
-      {conversations.map((conversation, index) => (
-        <ConversationList key={conversation._id} conversation={conversation} lastIndex={index === conversation.length - 1} />
-      ))}
-      {loading ? <span className="loading loading-spinner"></span> : null}
-    </div>
-  )
-}
-export default Conversations
-
-
-// import React from 'react';
-// import ConversationList from './ConversationList';
-// import useGetConversation from '../../hooks/useGetConversation';
+// import React from 'react'  
+// import ConversationList from './ConversationList'
+// import useGetConversation from '../../hooks/useGetConversation'
 
 // const Conversations = () => {
-//   const { loading, conversations } = useGetConversation();
+//   const {loading, conversations} = useGetConversation()
 //   console.log(conversations);
+  
 
 //   return (
 //     <div className='py-2 flex flex-col overflow-auto'>
-//       {[...conversations].map((conversation, index) => (
-//         <ConversationList key={conversation._id} conversation={conversation} lastIndex={index === conversations.length - 1} />
+//       {conversations.map((conversation, index) => (
+//         <ConversationList key={conversation._id} conversation={conversation} lastIndex={index === conversation.length - 1} />
 //       ))}
 //       {loading ? <span className="loading loading-spinner"></span> : null}
 //     </div>
-//   );
-// };
-// export default Conversations;
+//   )
+// }
+// export default Conversations
+
+
+import React from 'react';
+import ConversationList from './ConversationList';
+import useGetConversation from '../../hooks/useGetConversation';
+
+const Conversations = () => {
+  const { loading, conversations } = useGetConversation();
+  console.log(conversations);
+
+  return (
+    <div className="py-2 flex flex-col overflow-auto">
+      {Array.isArray(conversations) && conversations.map((conversation, index) => (
+        <ConversationList key={conversation._id} conversation={conversation} lastIndex={index === conversations.length - 1} />
+      ))}
+      {loading ? <span className="loading loading-spinner"></span> : null}
+    </div>
+  );
+};
+
+export default Conversations;
